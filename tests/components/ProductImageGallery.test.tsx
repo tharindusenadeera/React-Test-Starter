2b094993698abd36ec83382361cf061773c54336
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-08-10 12:39:28 +0100

    Image galary testing done

---
+++ tests/components/ProductImageGallery.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import ProductImageGallery from '../../src/components/ProductImageGallery';
+
+describe('ProductImageGallery', () => {
+  it('should render nothing if given array is empty', () => {
+    const { container } = render(<ProductImageGallery imageUrls={[]} />);
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  it('should render nothing if given array is empty', () => {
+    const { container } = render(<ProductImageGallery imageUrls={[]} />);
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  it('should render a list of images', () => {
+    const imageUrls = ['url1', 'url2'];
+
+    render(<ProductImageGallery imageUrls={imageUrls} />);
+
+    const images = screen.getAllByRole('img');
+    expect(images).toHaveLength(2);
+    imageUrls.forEach((url, index) => {
+      expect(images[index]).toHaveAttribute('src', url);
+    });
+  });
+});
