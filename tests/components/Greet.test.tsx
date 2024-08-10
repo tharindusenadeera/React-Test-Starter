7f4ebf659edb8b7c96bc6c63021156c6bd81d637
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-08-10 10:04:45 +0100

    Greet testing done

--- tests/components/Greet.test.tsx
+++ tests/components/Greet.test.tsx
@@ -1,11 +1,22 @@
 import { it, expect, describe } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Greet from '../../src/components/Greet';
+import '@testing-library/jest-dom/vitest';
 
 describe('group', () => {
   it('should render hello with the name when name is provided', () => {
     render(<Greet name="Tharindu" />);
 
-    screen.debug();
+    const heading = screen.getByRole('heading');
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/tharindu/i);
+  });
+
+  it('should render login button when name is not provided', () => {
+    render(<Greet />);
+
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/login/i);
   });
 });
